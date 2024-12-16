32171ae4125943bbb70785423e48d0621daa2068
HrpperCroFT <hrppercroft@gmail.com>
2024-12-17 01:09:20 +0300

    Added some test in synchronization

--- Assets/Printer.cs
+++ Assets/Printer.cs
@@ -3,10 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Printer : MonoBehaviour
+public class Printer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    public void Click() {
-        Debug.Log("clicked");
+    public int local = 0;
+    public int opposite = 0;
+
+    private PhotonView photonView;
+
+    private void Start() {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // привязывается к кнопке
+    public void OnButtonClick() {
+        local += 1;
+        
+        photonView.RPC(nameof(SyncScore), RpcTarget.Others, local);
+
+        Debug.Log("Local is: " + local.ToString());
+    }
+
+    [PunRPC]
+    private void SyncScore(int newGenValue) {
+        opposite = newGenValue;
+        Debug.Log("Opposite is: " + opposite.ToString());
     }
 }
