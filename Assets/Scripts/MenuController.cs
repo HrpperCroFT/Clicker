341fde19a4d0105562a45244fe01d7ffac00a9b2
dimapchik <prokopev.ds@phystech.edu>
2024-12-17 15:43:19 +0300

    add slider

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,15 +8,19 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField] private Button newGameButton;
     [SerializeField] private Button joinGameButton;
+    [SerializeField] private Slider seedSlider;
+    [SerializeField] private Text seedText;
 
     private void Start()
     {
         newGameButton.onClick.AddListener(OnNewGameClick);
         joinGameButton.onClick.AddListener(OnJoinGameClick);
+        seedSlider.onValueChanged.AddListener(OnSeedSliderChanged);
     }
 
     private void OnNewGameClick()
     {
+        PlayerPrefs.SetInt("Seed", (int)seedSlider.value);
         // NetworkManager.Singleton.StartHost();
         SceneManager.LoadScene("Game");
     }
@@ -24,6 +28,12 @@ public class MenuController : MonoBehaviour
     private void OnJoinGameClick()
     {
         // NetworkManager.Singleton.StartClient();
+    }
+
+    private void OnSeedSliderChanged(float value)
+    {
+        int seed = (int)value;
+        seedText.text = seed.ToString();
     }
 
     private void OnDestroy()
